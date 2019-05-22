0f51007468f81f0e905ca40141c5b65b862da055
kevin-huet <kevin.huet@epitech.eu>
2019-05-22 13:06:35 +0200

    add article table

--- epihub/src/Controller/HomeController.php
+++ epihub/src/Controller/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Epihub;
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
 use App\Form\RegisterFormType;
 use App\Form\LoginFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,27 +13,47 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    public function __construct(PropertyRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
     /**
      * @Route("/", name="home")
      * @return Response
      */
     public function index(): Response
     {
-        //$login = $this->createForm(LoginFormType::class);
-        //$register = $this->createForm(RegisterFormType::class);
-        /*$epihub = new Epihub();
-        $epihub->setTitle('guest')
-            ->setUsername('name')
-            ->setPassword('pass')
-            ->setMail('mail');
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($epihub);
-        $em->flush();*/
+        $property = $this->repository->find(1);
+        dump($property);
         return $this->render('pages/index.html.twig', [
             'current_menu' => 'home'
-            //'login' => $login->createView(),
-            //'register' => $register->createView()
         ]);
+    }
+    /**
+     * @Route("/article", name="article")
+     */
+    public function article(): Response
+    {
+        $repository = $this->getDoctrine()->getRepository(Property::class);
+        dump($repository);
+        return $this->render('pages/index.html.twig', [
+            'current_menu' => 'home'
+        ]);
+    }
+
+    /**
+     * @Route("/admin/article/create", name="admin.new")
+     */
+    public function new()
+    {
+        $property = new Property();
+        $property->setTitle('test')
+            ->setAuthor('test')
+            ->setDescription('blablabla');
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($property);
+        $em->flush();
     }
 
     /**
